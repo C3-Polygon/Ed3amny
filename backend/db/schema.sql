2f07d940cf38b86edc8ca43d4810b161d90833ec
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-10-07 21:33:41 +0300

    create User Table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,9 +1,17 @@
--- USE MERAKI_Academy_Project_5;
-
+USE MERAKI_Academy_Project_5;
 
 -- User
 
-
+CREATE TABLE users(
+    id INT AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    age INT(3) NOT NULL,
+    img VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 -- Categories
 
