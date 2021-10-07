25c8e57e753390d63437d413880a792ea35a6c86
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-08 00:53:25 +0300

    fix Sql Schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,6 +1,6 @@
 USE sql11442892 ;
 
--- User
+-- users
 
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT NOT NULL,
@@ -13,23 +13,44 @@ CREATE TABLE IF NOT EXISTS users(
     PRIMARY KEY (id)
 );
 
--- Categories
-
-CREATE TABLE Categories(
-id INT AUTO_INCREMENT NOT NULL,
-name VARCHAR(255) NOT NULL,
-Primary key ( id)
+--categories
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    Primary key (id)
 );
 
--- FundraiserPost
+--campaigns
+CREATE TABLE IF NOT EXISTS campaigns (
+    id INT AUTO_INCREMENT NOT NULL,
+    userid INT,
+    country VARCHAR(255) NOT NULL,
+    type INT NOT NULL,
+    target INT NOT NULL,
+    img VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    is_deleted TINYINT DEFAULT 0,
+    FOREIGN KEY (type) REFERENCES categories(id),
+    FOREIGN KEY (userid ) REFERENCES users(id),
+    Primary key (id)
+);
 
+--contributions
+CREATE TABLE IF NOT EXISTS contributions(
+    id INT AUTO_INCREMENT NOT NULL,
+    user_id INT,
+    campaign_id INT,
+    category_id INT,
+    created_at DATETIME,
+    amount INT,
+    FOREIGN KEY (campaign_id ) REFERENCES campaigns(id),
+    FOREIGN KEY (user_id ) REFERENCES users(id),
+    FOREIGN KEY (category_id ) REFERENCES categories(id),
+    Primary key (id)
+);
 
-
--- Contributions
-
-
-
--- BloodPost
+--bloodpost
 CREATE TABLE IF NOT EXISTS bloodpost(
     id INT AUTO_INCREMENT NOT NULL,
     user_id INT,
@@ -39,8 +60,14 @@ CREATE TABLE IF NOT EXISTS bloodpost(
     PRIMARY KEY (id)
 );
 
-
-
-
--- HospitalTable
+--HospitalTable
+CREATE TABLE IF NOT EXISTS HospitalTable(
+    id INT AUTO_INCREMENT NOT NULL,
+    Name VARCHAR(255) NOT NULL,  
+    user_id INT,
+    Amount INT NOT NULL,
+    BankAccount VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id ) REFERENCES users(id),
+    PRIMARY KEY (id)
+);
 
