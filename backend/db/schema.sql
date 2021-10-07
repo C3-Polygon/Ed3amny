c839636ca1a7a768c522cfa7c0aefe1bf9c20543
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-07 21:01:26 +0300

    Added Schema, connected db and server running

---
+++ backend/db/schema.sql
@@ -0,0 +1,25 @@
+-- USE MERAKI_Academy_Project_5;
+
+
+-- User
+
+
+
+-- Categories
+
+
+
+-- FundraiserPost
+
+
+
+-- Contributions
+
+
+
+-- BloodPost
+
+
+
+-- HospitalTable
+
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/db");
+
+//routers
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+//app routers
+
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server On ${PORT}`);
+});
