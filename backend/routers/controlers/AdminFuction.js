74444e7b5323a51b77aea0d8eebfcbbf001d32f2
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-10-08 02:16:17 +0300

    get all Fundraiser

--- backend/routers/controlers/AdminFuction.js
+++ backend/routers/controlers/AdminFuction.js
@@ -1,15 +1,16 @@
+const res = require("express/lib/response");
 const connection = require("../../db/db");
 
 //// Get All users
 
-const GetAllUser = () => {
-    const query = `SELECT * FROM users`;                                   
+const GetAllUser = (req, res) => {
+    const query = `SELECT * FROM users`;
     connection.query = (query, (err, result) => {
         if (err) {
             const error = {
                 success: false,
                 message: "Somthing Error Try Again",
-                err:err
+                err: err
             }
             res.json(error);
             res.status(500);
@@ -23,6 +24,33 @@ const GetAllUser = () => {
             res.json(success);
             res.status(200);
 
+        }
+    })
+}
+
+
+// get all Fundraiser
+
+
+const GetAllFundraiser = (req, res) => {
+    const query = `SELECT * FROM campaigns`;
+    connection.query = (query, (err, result) => {
+        if (err) {
+            const error = {
+                success: false,
+                message: err
+            }
+            res.json(error);
+            res.status(500);
+        }
+        if (result) {
+            const success = {
+                success: true,
+                message: "All Fundraiser",
+                Fundraisers: result
+            }
+            res.json(success);
+            res.status(200);
         }
     })
 }
