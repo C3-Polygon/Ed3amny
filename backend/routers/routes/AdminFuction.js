10dfb415b3d2983187a06f696c6f90b1ad6b94fd
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-10-08 23:42:55 +0300

    create comment for route path and show http method for it

--- backend/routers/routes/AdminFuction.js
+++ backend/routers/routes/AdminFuction.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const { GetAllUser, GetAllFundraiser, GetAllPendingPost, deleteFundraisers, AcceptFundraisers, rejectedTheFunders } = require("../controllers/AdminFuction");
 
-
+///const 
 const adminRouter = express.Router();
 
+///  [get]  [http://localhost:5000/admin]
 adminRouter.get('/', GetAllUser);
+
+///  [get]  [http://localhost:5000/admin/fundraiser]
 adminRouter.get('/fundraiser', GetAllFundraiser);
+
+///  [get]  [http://localhost:5000/admin/pending]
 adminRouter.get('/pending', GetAllPendingPost);
+
+///  [put]  [http://localhost:5000/admin/:id]
 adminRouter.put('/:id', deleteFundraisers);
+
+///  [put]  [http://localhost:5000/admin/accept/:id]
 adminRouter.put('/accept/:id', AcceptFundraisers);
+
+///  [put]  [http://localhost:5000/admin/rejected/:id]
 adminRouter.put('/rejected/:id', rejectedTheFunders);
 
 
