29849bec97b96f7e10ed88c42ee051b429eeafac
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-10 07:11:35 +0300

    social media sharing many platfroms ready, need to create states which will have values of post/fundraiser and pass to the share section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import React, { useState } from 'react';
-import FacebookLogin from 'react-facebook-login';
-import { Card, Image } from 'react-bootstrap';
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+import { Card, Image } from "react-bootstrap";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
+import { FacebookIcon } from "react-share";
+import { Button, Container, Header, Segment, Grid } from "semantic-ui-react";
 
+function App() {
   const [login, setLogin] = useState(false);
   const [data, setData] = useState({});
-  const [picture, setPicture] = useState('');
-  
+  const [picture, setPicture] = useState("");
+
   const responseFacebook = (response) => {
     console.log(response);
     setData(response);
@@ -17,38 +25,48 @@ function App() {
     } else {
       setLogin(false);
     }
-  }
+  };
 
   return (
-   
-<div class="container">
-<Card style={{ width: '600px' }}>
-  <Card.Header>
-    {!login &&
-      <FacebookLogin
-        appId="1259903211090202"
-        autoLoad={true}
-        fields="name,email,picture"
-        scope="public_profile,user_friends"
-        callback={responseFacebook}
-        icon="fa-facebook" />
-    }
-    {login &&
-      <Image src={picture} roundedCircle />
-    }
-  </Card.Header>
-  {login &&
-    <Card.Body>
-      <Card.Title>{data.name}</Card.Title>
-      <Card.Text>
-        {data.email}
-      </Card.Text>
-    </Card.Body>
-  }
-</Card>
-</div>
+    <>
+      <div class="container">
+        <Card style={{ width: "600px" }}>
+          <Card.Header>
+            {!login && (
+              <FacebookLogin
+                appId="1259903211090202"
+                autoLoad={false}
+                fields="name,email,picture"
+                scope="public_profile,user_friends"
+                callback={responseFacebook}
+                icon="fa-facebook"
+              />
+            )}
+            {login && <Image src={picture} roundedCircle />}
+          </Card.Header>
+          {login && (
+            <Card.Body>
+              <Card.Title>{data.name}</Card.Title>
+              <Card.Text>{data.email}</Card.Text>
+            </Card.Body>
+          )}
+        </Card>
+      </div>
 
-
+      <>
+        <Container>
+          <Segment>
+            <FacebookShareButton
+              url="https://www.facebook.com/AhmadMuraish"
+              quote={"Easy Peasy Lemon Squeezy"}
+              hashtag="#facebookshare"
+            >
+              <FacebookIcon LogoFillColor="white" round={true}></FacebookIcon>
+            </FacebookShareButton>
+          </Segment>
+        </Container>
+      </>
+    </>
   );
 }
 
