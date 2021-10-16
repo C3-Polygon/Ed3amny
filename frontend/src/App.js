b141300c21e3995091117054ee457012382161c8
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-10-16 20:26:43 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,8 +18,8 @@ import Leader from "./components/Leader/Leader";
 import Random from "./components/Randomfundraisers/Random";
 import FundRaiserView from "./components/FundRaiserView/FundRaiserView"
 import CategoryByType from "./components/CategoryByType/CategoryByType";
-import AccountSettings from "./components/Header/Dropdown/AccountSettings/AccountSettings";
-import YourFundraisers from "./components/Header/Dropdown/YourFundraisers/YourFundraisers";
+import AccountSettings from "./components/Header/Dropdown/AccountSettings/accountSettings";
+import YourFundraisers from "./components/Header/Dropdown/YourFundraisers/yourFundraisers";
 import DonateForSpecific from "./components/Header/Dropdown/DonateForSpecific/DonateForSpecific";
 import Footer from "./components/Footer/Footer";
     // mshan allah
