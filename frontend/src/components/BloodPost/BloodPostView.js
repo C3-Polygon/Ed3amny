9c0d6f789bf0c014c93eeed99d2f8f13596d3dcb
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-17 01:00:24 +0300

    BloodPost rendering fine

--- frontend/src/components/BloodPost/BloodPostView.js
+++ frontend/src/components/BloodPost/BloodPostView.js
@@ -1,25 +1,39 @@
-import {React,useState,useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Card from "react-bootstrap/Card";
 import axios from "axios";
-
+import { MdBloodtype } from 'react-icons/md';
 
 function BloodPostView() {
   const [show, setShow] = useState(false);
+  const [bloodPosts, setBloodPosts] = useState([]);
+  let tokenSave = localStorage.getItem("token");
+  let userIdSave = localStorage.getItem("CurrentUserId");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
-
-
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/bloodpost/`, {
+        headers: {
+          Authorization: `Bearer ${tokenSave}`,
+        },
+      })
+      .then((result) => {
+        console.log("result here", result.data.Data);
+        setBloodPosts(result.data.Data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
       <>
-        <Button variant="primary" onClick={handleShow}>
-          View BloodPosts
+     
+        <Button variant="success"className="ViewButton" onClick={handleShow}>
+        < MdBloodtype className="BloodButton"/>
         </Button>
 
         <Offcanvas show={show} onHide={handleClose}>
@@ -27,17 +41,19 @@ function BloodPostView() {
             <Offcanvas.Title>Offcanvas</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-           
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={"test"} />
-            <Card.Body>
-          <Card.Title>{"test"}</Card.Title>
-         <Card.Text>{"test"}</Card.Text>
-          <Button variant="primary">Go watch Naruto</Button>
-        </Card.Body>
-            </Card>
-
-
+            {bloodPosts &&
+              bloodPosts.map((elem, index) => {
+                return (
+                  <Card key = {index} style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={elem.img} />
+                    <Card.Body>
+                      <Card.Title>{elem.title}</Card.Title>
+                      <Card.Text>{elem.descriptionn}</Card.Text>
+                      
+                    </Card.Body>
+                  </Card>
+                );
+              })}
           </Offcanvas.Body>
         </Offcanvas>
       </>
