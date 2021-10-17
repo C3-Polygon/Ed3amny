af00262d2d25b2fb13b5efd809056ea6f21eb53e
Alkiswani966 <maamounalkiswani1996@gmail.com>
2021-10-18 00:18:45 +0300

    change type for fundreisers

--- frontend/src/components/DashboradAdmin/ShowAllPost/ShowAllfundreiser.js
+++ frontend/src/components/DashboradAdmin/ShowAllPost/ShowAllfundreiser.js
@@ -5,17 +5,71 @@ import Table from 'react-bootstrap/Table';
 import User from '../getAllUsers/Users';
 import axios from 'axios';
 import { Button } from 'bootstrap';
+import { reject } from 'bcrypt/promises';
 
 const ShowAllfundreiser = () =>{
-    const [getfundraiser, setGetfundraiser] = useState();
+    const [getfundraiser, setGetfundraiser] = useState([]);
     
     useEffect(() => {
         axios.get(`http://localhost:5000/fundraiser/admin/dashbord/get/getallfundreiser`).then((result) => {
             setGetfundraiser(result.data.allData);
-        }).catch((err) => {
-            console.log(err);
-        },)
+            return;
+        }).catch((error) => {
+            throw error;
+        })
+        
       },[]);
+
+      const getallpost = () =>{
+        axios.get(`http://localhost:5000/fundraiser/admin/dashbord/get/getallfundreiser`).then((result) => {
+            setGetfundraiser(result.data.allData);
+            return;
+        }).catch((error) => {
+            throw error;
+        })
+      }
+
+    const Accsept = (id)=>{
+        axios.put(`http://localhost:5000/admin/accept/${id}`).then((result) => {
+            getallpost();
+        }).catch((err) => {
+            throw err;
+            
+        })
+    }
+
+    const reject =(id)=>{
+        axios.put(`
+        http://localhost:5000/admin/rejected/${id}`).then((result) => {
+            getallpost();
+        }).catch((err) => {
+            throw err;
+            
+        })
+    }
+
+    const pending = (id)=>{
+        axios.put(`
+        http://localhost:5000/admin/batataa/batata/pending/${id}`).then((result) => {
+            getallpost();
+        }).catch((err) => {
+            throw err;
+            
+        })
+
+    }
+
+    const ChangeFundraiserType = (id , typepost)=>{
+            if(typepost === 0){
+                Accsept(id);
+            }else if(typepost === 1){
+                reject(id);
+            }else{
+                pending(id);
+            }
+    }
+
+
     return (
         <>
         
@@ -29,20 +83,20 @@ const ShowAllfundreiser = () =>{
                             <tr>
                             <th>#id</th>
                             <th>Fundraisers title </th>
-                            <th>Email</th>
+                            <th>update fundraiser</th>
                             <th>Status</th>
                             </tr>
                         </thead>
                         <tbody>
                             
-                            {getfundraiser&&getfundraiser.map((post)=>{
+                            {getfundraiser&&getfundraiser.map((post ,index)=>{
                                 return(
                                     <>
-                                    <tr>
+                                    <tr key={index}>
                                     <td>{post.id}</td>
                                     <td>{post.title}</td>
-                                    <td>{post.email}</td>
-                                    <td>{ post.is_deleted === 0 ? <button className='pending'>in process</button> : post.is_deleted === 1 ? <button className='approved'>approved</button> : post.is_deleted === 2 ? <button className='deleted'>deleted</button> : console.log("UnKnown")} </td>
+                                    <td>Empty</td>
+                                    <td>{ post.is_deleted === 0 ? <button className='pending' onClick={()=>{ChangeFundraiserType(post.id , 0)}}>in process</button> : post.is_deleted === 1 ? <button className='approved' onClick={()=>{ChangeFundraiserType(post.id ,1 )}}>approved</button> : post.is_deleted === 2 ? <button className='deleted' onClick={()=>{ChangeFundraiserType(post.id , 2)}}>deleted</button> : console.log("UnKnown")} </td>
                                     </tr>
                                     </>
                                 )
