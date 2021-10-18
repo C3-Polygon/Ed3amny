7774ba7498aad3bd698b19a3b0d3c9bee4e89186
hushki94 <omarhushki94@gmail.com>
2021-10-19 02:37:25 +0300

    get the post in the edit component

--- frontend/src/components/Header/Dropdown/YourFundraisers/EditFundraiser.js
+++ frontend/src/components/Header/Dropdown/YourFundraisers/EditFundraiser.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { React, useState, useEffect ,  } from "react";
 import { useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; 
+import "./EditFundraiser.css"
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './YourFundraisers.css';
@@ -14,6 +15,29 @@ import axios from "axios";
 
 export const EditFundraiser = ()=>{
     const {id} = useParams()
+    const [post , setPost ] = useState()
+    
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/fundraiser/id/${id}`).then((result)=>{
+            console.log("result",result)
+            setPost(result.data.result);
+        }).catch((err)=>{
+            console.log(err)
+        })
 
-    return (<div>{id}</div>)
+    })
+
+    return (
+    <div className='main-update'>
+        <div className='container'>
+         <button>Back</button>
+         <div>{post&&post.map((elm,i)=>{
+             return (<div key={i}>
+                 {elm.title}
+                 <img src={elm.img}/>
+                 </div>)
+         })}</div>
+         
+        </div>
+    </div>)
 }
