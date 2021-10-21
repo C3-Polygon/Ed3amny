e98f146cee8e6cecbd3c0a36544486ed6bb75fb1
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-10-21 22:39:58 +0300

    make the email forgot password and test it

--- frontend/src/components/services/ForgotPassword/ForgotMain/ForgotMain.js
+++ frontend/src/components/services/ForgotPassword/ForgotMain/ForgotMain.js
@@ -1,38 +1,68 @@
 import React, { useState } from "react";
 import "./ForgotMain.css";
-
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const ForgotMainPage = () => {
-const [emailCheck , setEmailCheck] = useState("")
-const [showEmailCheck , setShowEmailCheck] = useState(false)
-const [smsCheck , setSmsCheck] = useState("")
-const [showSmsCheck , setShowSmsCheck] = useState(false)
+    const history = useHistory();
+  const [emailCheck, setEmailCheck] = useState("");
+  const [showEmailCheck, setShowEmailCheck] = useState(false);
+  const [smsCheck, setSmsCheck] = useState("");
+  const [showSmsCheck, setShowSmsCheck] = useState(false);
+
   const emailCheckForSend = () => {
-    setShowEmailCheck(true)
- console.log("showEmailCheck" ,showEmailCheck)
+    setShowEmailCheck(true);
+    console.log("showEmailCheck", showEmailCheck);
   };
 
-console.log("emailCheck" , emailCheck);
   const smsCheckForSend = () => {
-    setShowSmsCheck(true)
- console.log("showSmsCheck" ,showSmsCheck)
-
+    setShowSmsCheck(true);
+    console.log("showSmsCheck", showSmsCheck);
   };
- const sendEmail = () =>{
-
-  }
-
+  const sendEmail = (e) => {
+    console.log("i am here");
+    e.preventDefault();
+    let email = emailCheck;
+    console.log(email);
+    axios
+      .post("http://localhost:5000/forgetPassword", { email })
+      .then((result) => {
+        console.log("result", result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setEmailCheck("");
+    history.push('/login')
+  };
+  console.log("showEmailCheck 2", showEmailCheck);
   return (
     <>
-      
-       {showEmailCheck?( <div className="errorMessage"><br/> <h1> we will send a new password <br/> on your email " {emailCheck} " </h1> 
-       <input placeholder='Enter Your Email' onChange={(e)=>{setEmailCheck(e.target.value)}} /> 
-       <button onClick={sendEmail}> Send </button> {showEmailCheck}</div>):(
-            <div> 
-            <button onClick={emailCheckForSend}> email help</button>
-            <button onClick={smsCheckForSend}> SMS</button>
-            </div> 
-       )}
-     
+      {showEmailCheck ? (
+        <div className="errorMessage">
+          <br />{" "}
+          <h1>
+            {" "}
+            we will send a new password <br /> on your email " {
+              emailCheck
+            } "{" "}
+          </h1>
+          <input
+            placeholder="Enter Your Email"
+            onChange={(e) => {     
+              setEmailCheck(e.target.value);
+            }}
+          />
+          <button onClick={sendEmail} type="submit">
+            {" "}
+            Send{" "}
+          </button>{" "}
+        </div>
+      ) : (
+        <div>
+          <button onClick={emailCheckForSend}> email help</button>
+          <button onClick={smsCheckForSend}> SMS</button>
+        </div>
+      )}
     </>
   );
 };
