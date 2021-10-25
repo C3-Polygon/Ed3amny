a4195d598f85e4c4446b6b174d5ce39d1d0feecf
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-25 09:57:51 +0300

    small error fix, updated online db with fundraisers and images

--- frontend/src/components/Header/Dropdown/Fundraiser/CreatefundRaiser.js
+++ frontend/src/components/Header/Dropdown/Fundraiser/CreatefundRaiser.js
@@ -129,7 +129,7 @@ const CreatefundRaiser = (e) => {
 
   <Form.Group className="mb-3" controlId="formGridAddress2">
     <Form.Label>Your Fundraiser description </Form.Label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Here ..." rows="3" onChange={(e)=>{setDescriptionn(e.target.value)}} maxlength="245" ></textarea>
+    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Here ..." rows="3" onChange={(e)=>{setDescriptionn(e.target.value)}} maxLength="245" ></textarea>
   </Form.Group>
 
   <Row className="mb-3">
