053e5021e7d9dfeaabf9bbb8949fa0ded617247d
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-26 21:36:30 +0300

    deploy

---
+++ client/src/components/Auth/signup/signup.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import "./signup.css";
+// import Col from 'react-bootstrap/Col';
+
+const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [img, setImg] = useState("");
+  const [email, setEmail] = useState("");
+  const [country, setCountry] = useState("");
+  const [passwordd, setPasswordd] = useState("");
+
+  const history = useHistory();
+  //Messgae error
+
+  // const [fieldfristName , setFieldfristName] = useState('');
+  // const [fieldlastName , setFieldlastName] = useState('');
+  // const [fieldage , setFieldage] = useState('');
+  // const [fieldimg , setFieldimg] = useState('');
+  // const [fieldemail , setFieldemail] = useState('');
+  // const [fieldcountry , setFieldcountry] = useState('');
+  // const [fieldpassword , setFieldpassword] = useState('');
+
+  const insertNewUser = (e) => {
+    e.preventDefault();
+    const theUsers = {
+      firstName,
+      email,
+      country,
+      passwordd,
+    };
+    axios
+      .post("/signup", theUsers)
+      .then((result) => {
+        history.push("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className="Main-SignUp">
+        <div className="form-signup">
+          <h3 className="title-form">Sign Up </h3>
+          <hr></hr>
+          <Form onSubmit={insertNewUser}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="E-mail"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="Country"
+                onChange={(e) => {
+                  setCountry(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPasswordd(e.target.value);
+                }}
+              />
+            </Form.Group>
+
+            {/* <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group> */}
+
+            <button variant="primary" className="signup-btn" type="submit">
+              Next
+            </button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
