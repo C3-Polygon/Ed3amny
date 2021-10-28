786edce6e47cd1cea7751bc4b426c4548130ce1d
AhmadMraish <ahmadmraish59@gmail.com>
2021-10-28 09:03:30 +0300

    React Tour Implementation on how to create a fundraiser

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,16 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import {persistor} from './reducers/index'
+import { JourneyComponent, JourneyProvider } from 'react-journey';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
    <React.StrictMode>
    <PersistGate persistor={persistor}>
+   <JourneyProvider Component={JourneyComponent}>
      <App />
+     </JourneyProvider>,
      </PersistGate>
    </React.StrictMode>
 
